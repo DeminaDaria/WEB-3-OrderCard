173d8805bd0306df4f6dba9419724fc6500c1d20
DeminaDaria <61200143+DeminaDaria@users.noreply.github.com>
2021-03-14 11:21:13 +0300

    update build.gradle, changed webDriver

--- src/test/java/OrderCardForm.java
+++ src/test/java/OrderCardForm.java
@@ -1,10 +1,8 @@
-import io.github.bonigarcia.seljup.SeleniumJupiter;
 import io.github.bonigarcia.wdm.WebDriverManager;
 import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
-import org.junit.jupiter.api.extension.ExtendWith;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
@@ -13,7 +11,6 @@ import org.openqa.selenium.chrome.ChromeOptions;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
-@ExtendWith(SeleniumJupiter.class)
 
 public class OrderCardForm {
     private WebDriver driver;
